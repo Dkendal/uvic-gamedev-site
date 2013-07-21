1db4db22422bb7dcbfe58a542dfa661b3e290566
= <=>
2013-07-21 14:29:50 -0700

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 GameDev::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
