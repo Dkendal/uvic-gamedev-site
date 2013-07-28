0ad13d82136e348cc66484e51b7c06259a0fabc1
= <=>
2013-07-28 13:06:22 -0700

    Revert "ignore secret token!"
    This reverts commit 1bf74fe679216375a83d5a6d3730298dba480ba5.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameDev::Application.config.secret_key_base = 'b81f1c49bf2a0c5a1acac10703910a61246784f835b29f71398c375f627830db06b94882e5b995bb861036e0a60ee5d52d379dcf10d053dd876a5395e7a5eb6d'
