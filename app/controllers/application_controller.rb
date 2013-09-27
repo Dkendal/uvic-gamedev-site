71e2c5275e0fa7a951152921bc4e377399129f4f
= <dylankendal@gmail.com>
2013-09-26 22:37:19 -0700

    redirect on authentication failure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to :back, alert: exception.message
+    if user_signed_in?
+      redirect_to :back, alert: exception.message
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
