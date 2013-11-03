0fae305fd65a5c4aac470e4c05457192a794a235
= <dylankendal@gmail.com>
2013-11-03 14:51:02 -0800

    put optional attr's in event factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,7 +3,12 @@
 FactoryGirl.define do
   factory :event do
     user
-    start_date Time.now
+    start_date "2014/01/20"
+    end_date "2014/01/20"
+    start_time "12:00"
+    end_time "14:00"
+    location "Uvic"
+    description "a lil' diddy"
     name "an event"
   end
 end
